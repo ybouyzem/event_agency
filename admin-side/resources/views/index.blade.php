48bffc52a5e50ddee7f403c0ddeab42057c5ce3d
ybouyzem <alpha0110@hotmail.com>
2024-05-03 17:06:59 +0100

    data base commands

--- admin-side/resources/views/index.blade.php
+++ admin-side/resources/views/index.blade.php
@@ -63,7 +63,7 @@
 				</a>
 			</li> -->
 			<li>
-				<a href="#" class="logout">
+				<a href="" class="logout">
 					<i class='bx bxs-log-out-circle' ></i>
 					<span class="text">Logout</span>
 				</a>
