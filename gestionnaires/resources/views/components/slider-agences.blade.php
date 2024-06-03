3f5d36dd10e1e58cd131d520e5c23b560c7bfcb5
Younes Bouyzem <ybouyzem@e1r9p2.1337.ma>
2024-06-03 13:24:25 +0100

    starting favoit page

--- gestionnaires/resources/views/components/slider-agences.blade.php
+++ gestionnaires/resources/views/components/slider-agences.blade.php
@@ -1,7 +1,6 @@
 @props(['gestionnaires'])
-
+  
   @if (empty($gestionnaires))
-
     <div class="empty-gest">
       Désolé! il n'y a aucune agence ou prestataire disponible actuellement.
     </div>
@@ -19,7 +18,7 @@
             <img src="img/{{$gestionnaire->image1}}" alt="">
           </div>
           <label class="favorite">
-            <input checked type="checkbox">
+            <input type="checkbox">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#000000">
               <path d="M12 20a1 1 0 0 1-.437-.1C11.214 19.73 3 15.671 3 9a5 5 0 0 1 8.535-3.536l.465.465.465-.465A5 5 0 0 1 21 9c0 6.646-8.212 10.728-8.562 10.9A1 1 0 0 1 12 20z"></path>
             </svg>
--- gestionnaires/resources/views/profile-client.blade.php
+++ gestionnaires/resources/views/profile-client.blade.php
@@ -1,3 +1,4 @@
+@props(['gestionnaires'])
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -23,6 +24,55 @@
   </head>
   <body>
     <x-header-client/>
-    <a href="{{route('logout')}}" style="font-size: 30px;position: absolute; top:300px">log out</a>
+    {{-- <a href="{{route('logout')}}" style="font-size: 30px;position: absolute; top:300px">log out</a> --}}
+    <section class="profile-container">
+      <div class="sidebar-client">
+        <ul>
+          <li><a href="">Mon Compte</a></li>
+          <li><a href="">Historique des reservations </a></li>
+          <li><a href="">Ma liste d'envie</a></li>
+          <li><a href="">Déconnexion</a></li>
+        </ul>
+      </div>
+      <div class="main-client">
+        <h2>Ma liste d'envie</h2>
+        <div class="card-agence">
+          <div class="image-container">
+            <img src="img/{{$gestionnaire->image1}}" alt="">
+          </div>
+          <label class="favorite">
+            <input type="checkbox">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#000000">
+              <path d="M12 20a1 1 0 0 1-.437-.1C11.214 19.73 3 15.671 3 9a5 5 0 0 1 8.535-3.536l.465.465.465-.465A5 5 0 0 1 21 9c0 6.646-8.212 10.728-8.562 10.9A1 1 0 0 1 12 20z"></path>
+            </svg>
+          </label>
+          <div class="content">
+            <div class="brand">{{$gestionnaire->nom}}</div>
+            <div class="other-data">
+              <div class="product-name" >Type: <span id="type-gest">{{$gestionnaire->type}}</span></div>
+              <div class="product-name">Service: <span id="type-service">{{$gestionnaire->service}}</span></div>
+              <div class="product-name">Prix A partir de : <span id="price"> {{$gestionnaire->prix}}</span> dh</div>
+            </div>
+            <div class="rating">
+              <svg viewBox="0 0 99.498 16.286" xmlns="http://www.w3.org/2000/svg" class="svg four-star-svg">
+                <path fill="#fc0" transform="translate(-0.001 -1.047)" d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z" id="star-svgrepo-com"></path>
+                <path fill="#fc0" transform="translate(20.607 -1.047)" d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z" data-name="star-svgrepo-com" id="star-svgrepo-com-2"></path>
+                <path fill="#fc0" transform="translate(41.215 -1.047)" d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z" data-name="star-svgrepo-com" id="star-svgrepo-com-3"></path>
+                <path fill="#fc0" transform="translate(61.823 -1.047)" d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z" data-name="star-svgrepo-com" id="star-svgrepo-com-4"></path>
+                <path fill="#e9e9e9" transform="translate(82.431 -1.047)" d="M9.357,1.558,11.282,5.45a.919.919,0,0,0,.692.5l4.3.624a.916.916,0,0,1,.509,1.564l-3.115,3.029a.916.916,0,0,0-.264.812l.735,4.278a.919.919,0,0,1-1.334.967l-3.85-2.02a.922.922,0,0,0-.855,0l-3.85,2.02a.919.919,0,0,1-1.334-.967l.735-4.278a.916.916,0,0,0-.264-.812L.279,8.14A.916.916,0,0,1,.789,6.576l4.3-.624a.919.919,0,0,0,.692-.5L7.71,1.558A.92.92,0,0,1,9.357,1.558Z" data-name="star-svgrepo-com" id="star-svgrepo-com-5"></path>
+              </svg>
+              (29,062)
+            </div>
+          </div>
+          <div class="button-container">
+            <button class="buy-button button" onclick="showDetailGest({{ $gestionnaire->id }})">Lire Plus</button>
+            @if (session('client'))
+              <button class="contactbtn"><i class='bx bxs-phone-call' ></i></button>
+            @endif
+          </div>
+        </div>
+      </div>
+    </section>
+
   </body>
 </html>
